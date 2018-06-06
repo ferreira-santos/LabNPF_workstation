55c3c2c889f098d5edb79304ddf15c07f0c0a560
Fernando Ferreira-Santos <ferreira-santos@users.noreply.github.com>
2018-06-06 12:44:20 +0100

    Add files via upload

---
+++ LabNPF_workstation/matlab_toolboxes/startup.m
@@ -0,0 +1,10 @@
+% Matlab startup routines for LabNPF workstations
+%
+% Note: The folder where this file (startup.m) is located must be 
+%       added to the Matlab path (typically 
+%       "C:\LabNPF_workstation\matlab_toolboxes\").
+%
+
+% 1) Call "labnpf_select_eeglab_version.m" - 
+labnpf_select_eeglab_version('C:\LabNPF_workstation\matlab_toolboxes\');
+
